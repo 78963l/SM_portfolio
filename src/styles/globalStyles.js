00d77ecb2bc27e7fc12bc23077a92a1d7d9513c5
78963l <78963l@naver.com>
2023-12-29 05:11:02 +0900

    globalStyle 셋팅

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,64 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+    *, *::before, *::after {
+        font-family: 'Pretendard' !important;
+        font-weight: 400;
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    /* *:focus{
+        outline: 2px dashed black;
+    } */
+    html {
+        width : 100%;
+        height: 100%;
+    }
+    body{
+        max-width: 100%;
+        height: 100%;
+        background-image: none;
+        background-repeat: no-repeat;
+        background-position: center;
+        box-sizing: border-box;
+    };
+    ul, ol {
+    list-style: none;
+    };
+    a {
+    text-decoration: none;
+    };
+    button{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        outline: none;
+        border : none;
+        background : none;
+        border-radius: 3px;
+    };
+    input{
+        display: flex;
+        outline: none;
+        padding-left: 10px;
+    }
+    label{
+        cursor: pointer;
+    }
+    strong{
+        font-weight: 800 !important;
+    }
+    em{
+        font-style: italic !important;
+    }
+
+    //테이블 상하 중앙 정렬
+    thead, tbody, tfoot { vertical-align: middle }
+    td, th, tr { vertical-align: inherit }
+`;
+
+export default GlobalStyles;
